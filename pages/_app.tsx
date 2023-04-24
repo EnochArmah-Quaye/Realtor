fb7609773196b56285bbd2992b31fc0973f6c432
Enoch Armah-Quaye <armahquayeenoch494@gmail.com>
2023-04-24 21:00:50 +0000

    4
    3

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import Router from 'next/router';
+import Head from 'next/head';
+import NProgress from 'nprogress';
+import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app'
 
+import Layout from '../components/Layout';
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+
+      </Head>
+      <ChakraProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
+    </>
+  )
 }

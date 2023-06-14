87ab8bf58ada30468922eec0701f5f337e350d56
Enoch Armah-Quaye <armahquayeenoch494@gmail.com>
2023-06-14 13:37:18 +0000

    filterData
    1

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -1,14 +1,27 @@
 import { useEffect, useState } from 'react';
-import { Flex, Select, Box, Text, Input, Spinner, Icon, Button } from '@chakra-ui/react';
+import { Flex, Select, Box, Text, Input, Spinner, Icon, Button, filter } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
 import { MdCancel } from 'react-icons/md';
 import Image from 'next/image';
 
+import { filterData, getFilterValues } from '../utils/filterData'
+
 const SearchFilters = () => {
+    const [filters, setfilters] = useState({filterData});
+
+    const searchProperties = (filterValues) => {
+      
+    }
  return (
-    <div>
-        SearchFilters
-    </div>
+    <Flex bg="gray.100" p="4" justifyContent="center" flexWrap="wrap">
+      {filter.map((filter) => (
+         <Box key={filter.queryName}>
+            <Select onChange={(e) => searchProperties()}>
+
+            </Select>
+         </Box>
+      ))}
+    </Flex>
  )
 } 
 
